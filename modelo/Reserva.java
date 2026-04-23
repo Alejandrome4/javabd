4041ae3991eb4723d98a827d06e4856e93ff9649
Alejandrome4 <alemaresc@alu.edu.gva.es>
2026-04-23 09:39:36 +0200

    Add files via upload

--- modelo/Reserva.java
+++ modelo/Reserva.java
@@ -1,32 +1,15 @@
 package modelo;
 
-import java.time.LocalDate;
+import java.sql.Date;
+
 
 public class Reserva {
     private int idReserva;
-    private String correoUsuario;
-    private int idRecurso;
-    private LocalDate fechaReserva;
+    private Usuario usuario;
+    private Recurso recurso;
+    private Date fechaReserva;
     private String comentarios;
 
-    public Reserva() {}
-
-    public Reserva(int idReserva, String correoUsuario, int idRecurso, LocalDate fechaReserva, String comentarios) {
-        this.idReserva = idReserva;
-        this.correoUsuario = correoUsuario;
-        this.idRecurso = idRecurso;
-        this.fechaReserva = fechaReserva;
-        this.comentarios = comentarios;
-    }
-
-    public int getIdReserva() { return idReserva; }
-    public void setIdReserva(int idReserva) { this.idReserva = idReserva; }
-    public String getCorreoUsuario() { return correoUsuario; }
-    public void setCorreoUsuario(String correoUsuario) { this.correoUsuario = correoUsuario; }
-    public int getIdRecurso() { return idRecurso; }
-    public void setIdRecurso(int idRecurso) { this.idRecurso = idRecurso; }
-    public LocalDate getFechaReserva() { return fechaReserva; }
-    public void setFechaReserva(LocalDate fechaReserva) { this.fechaReserva = fechaReserva; }
-    public String getComentarios() { return comentarios; }
-    public void setComentarios(String comentarios) { this.comentarios = comentarios; }
+    public Reserva () {}
+    public Reserva (Usuario ususario, Recurso recurso, Date fechaReserva, )
 }
