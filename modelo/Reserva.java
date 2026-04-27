c748294a29e7fe4485813185dd48ca460bf9794b
Alejandrome4 <alemaresc@alu.edu.gva.es>
2026-04-27 10:22:43 +0200

    Add files via upload

--- modelo/Reserva.java
+++ modelo/Reserva.java
@@ -10,6 +10,65 @@ public class Reserva {
     private Date fechaReserva;
     private String comentarios;
 
-    public Reserva () {}
-    public Reserva (Usuario ususario, Recurso recurso, Date fechaReserva, )
+    public Reserva() {
+    }
+
+    public Reserva(Usuario usuario, Recurso recurso, Date fechaReserva, String comentarios) {
+        this.usuario = usuario;
+        this.recurso = recurso;
+        this.fechaReserva = fechaReserva;
+        this.comentarios = comentarios;
+    }
+
+    public Reserva(int idReserva, Usuario usuario, Recurso recurso, Date fechaReserva, String comentarios) {
+        this.idReserva = idReserva;
+        this.usuario = usuario;
+        this.recurso = recurso;
+        this.fechaReserva = fechaReserva;
+        this.comentarios = comentarios;
+    }
+
+    public int getIdReserva() {
+        return idReserva;
+    }
+    public void setIdReserva(int idReserva) {
+        this.idReserva = idReserva;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Recurso getRecurso() {
+        return recurso;
+    }
+    public void setRecurso(Recurso recurso) {
+        this.recurso = recurso;
+    }
+
+    public Date getFechaReserva() {
+        return fechaReserva;
+    }
+
+    public void setFechaReserva(Date fechaReserva) {
+        this.fechaReserva = fechaReserva;
+    }
+    public String getComentarios() {
+        return comentarios;
+    }
+    public void setComentarios(String comentarios) {
+        this.comentarios = comentarios;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Reserva #" + idReserva + " [" + usuario.getNombre() + " -> " + recurso.getNombre() + " el " + fechaReserva + "]";
+    }
 }
+
+
